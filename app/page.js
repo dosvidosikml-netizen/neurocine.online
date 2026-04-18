aa83bbe77c372b69e136fad764c212963dc2a672
dosvidosikml-netizen <dosvidosikml@gmail.com>
2026-04-18 15:29:57 +0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,1 @@
-export default function Page() {
-  return (
-    <div style={{ padding: '50px', color: '#00ffcc', background: '#0a0a0a', height: '100vh', textAlign: 'center', fontFamily: 'sans-serif' }}>
-      <h1>🚀 NEUROCINE SERVER IS ALIVE!</h1>
-      <p>Если ты видишь этот текст, значит Render, домен и SSL работают ИДЕАЛЬНО.</p>
-    </div>
-  );
-}
+
