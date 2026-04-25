0808c212acb53f11900359cbcd6595bde6332c50
dosvidosikml-netizen <dosvidosikml@gmail.com>
2026-04-25 05:05:30 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   reactStrictMode: true,
   poweredByHeader: false,
   images: { unoptimized: true },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "70mb",
+    },
+  },
 };
 
 module.exports = nextConfig;
