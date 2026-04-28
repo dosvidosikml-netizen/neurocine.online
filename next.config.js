fad85ce3c80f4fdcebaab95d7306bc968f0d1abe
dosvidosikml-netizen <dosvidosikml@gmail.com>
2026-04-28 16:14:52 +0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  eslint: { ignoreDuringBuilds: true }
+};
+
 module.exports = nextConfig;
