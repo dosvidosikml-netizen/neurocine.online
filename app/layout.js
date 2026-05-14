e069ab8a45bbbc821b49791d108242639ea0df61
dosvidosikml-netizen <dosvidosikml@gmail.com>
2026-05-14 17:15:19 +0300

    feat(series): load series workspace styles

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,7 @@ import "./public-landing-v3.css";
 import "./style-engine-patch.css";
 import "./generation-overlay.css";
 import "./character-bible.css";
+import "./series-workspace.css";
 import AuthSignOutPatch from "../components/AuthSignOutPatch";
 import ScriptPolishPatch from "../components/ScriptPolishPatch";
 import GenerationCinematicOverlay from "../components/GenerationCinematicOverlay";
