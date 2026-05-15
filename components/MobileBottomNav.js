4bf4a7c8a69854d58e05888ed986fb1655e36795
dosvidosikml-netizen <dosvidosikml@gmail.com>
2026-05-15 21:29:34 +0300

    fix(nav): route mobile home to studio and studio to storyboard

--- components/MobileBottomNav.js
+++ components/MobileBottomNav.js
@@ -1,13 +1,25 @@
 "use client";
 
 export default function MobileBottomNav({ onCreate, onNavigate }) {
+  function goStudioHome() {
+    if (typeof window !== "undefined") window.location.href = "/studio";
+  }
+
+  function goStoryboard() {
+    if (typeof window !== "undefined") window.location.href = "/storyboard#storyboard";
+  }
+
+  function goPack() {
+    if (typeof window !== "undefined") window.location.href = "/studio#studio-package";
+  }
+
   return (
     <nav className="nc-bottom-nav" aria-label="NeuroCine mobile navigation">
-      <button type="button" onClick={() => onNavigate?.("setup")}><span>⌂</span><b>Главная</b></button>
+      <button type="button" onClick={goStudioHome}><span>⌂</span><b>Главная</b></button>
       <button type="button" onClick={() => onNavigate?.("projects")}><span>☷</span><b>Проекты</b></button>
       <button className="nc-create-plus" type="button" onClick={onCreate} aria-label="Создать">+</button>
-      <button type="button" onClick={() => onNavigate?.("production")}><span>▣</span><b>Studio</b></button>
-      <button type="button" onClick={() => onNavigate?.("pack")}><span>◉</span><b>Пакет</b></button>
+      <button type="button" onClick={goStoryboard}><span>▣</span><b>Studio</b></button>
+      <button type="button" onClick={goPack}><span>◉</span><b>Пакет</b></button>
     </nav>
   );
 }
