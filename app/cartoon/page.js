db77326db818e562f162a1e07b0e1e31ad928c7f
dosvidosikml-netizen <dosvidosikml@gmail.com>
2026-05-16 20:02:13 +0300

    revert(cartoon): remove neon top board experiment

--- app/cartoon/page.js
+++ app/cartoon/page.js
@@ -2,7 +2,6 @@ import CartoonStudioShell from "../../components/cartoon/CartoonStudioShell";
 import "../../public/cartoon/quantum.css";
 import "../../public/cartoon/quantum-fix.css";
 import "../../public/cartoon/quantum-clean-flow.css";
-import "../../public/cartoon/quantum-topboard-v2.css";
 
 export const metadata = {
   title: "Quantum Cartoon Creator · NeuroCine",
