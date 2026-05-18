2414865aead6627c0701eed7a6e7ce3c25250b6c
dosvidosikml-netizen <dosvidosikml@gmail.com>
2026-05-18 16:37:44 +0300

    feat(cartoon): mount frame 2x2 quick entry

--- components/cartoon/CartoonStudioShellV2.js
+++ components/cartoon/CartoonStudioShellV2.js
@@ -9,6 +9,7 @@ import CreateHub from "../CreateHub";
 import QuantumCartoonCreatorV2 from "./QuantumCartoonCreatorV2";
 import CartoonAutosaveBridge from "./CartoonAutosaveBridge";
 import CartoonScriptFallbackBridge from "./CartoonScriptFallbackBridge";
+import CartoonFrame2x2QuickBridge from "./CartoonFrame2x2QuickBridge";
 import { getAccountAccess, shouldForceLiveForAccount } from "../../lib/accountRoles";
 
 export default function CartoonStudioShellV2() {
@@ -203,6 +204,7 @@ export default function CartoonStudioShellV2() {
       <AuthPanel devMode={devMode} onAccountChange={setAccount} />
       <CartoonAutosaveBridge />
       <CartoonScriptFallbackBridge />
+      <CartoonFrame2x2QuickBridge />
 
       <div className="nc-cartoon-clean-start" aria-label="Cartoon clean start controls">
         <button type="button" onClick={cleanStart}>🧹 Новый проект</button>
